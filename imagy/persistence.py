0bceedea6fed26e9dc30dbc4fc0234a4acf87604
Dominik Dabrowski <dominik@silberrock.com>
2012-06-14 16:13:18 -0400

    before refactor

--- imagy/persistence.py
+++ imagy/persistence.py
@@ -1,29 +1,14 @@
-from config import STORE_LOC
 from tempfile import NamedTemporaryFile
 import shelve
 from path import path
 
 class Store(object):
     def __init__(self, loc=None):
-        self.loc = loc
         self.load(loc)
     
-    @property
-    def touched(self):
-        return self.d['set']
-    
-    @property
-    def originals(self):
-        return self.d['orig']
+    def __getattr__(self, name):
+        return getattr(self.d, name)
 
-    def fill(self):
-        for name, typ in (
-            ('set', set),
-            ('orig', dict),
-            ):
-            if not name in self.d:
-                self.d[name] = typ()
-    
     def load(self, loc=None):
         if loc is None:
             f = NamedTemporaryFile(delete=False)
@@ -32,17 +17,28 @@ class Store(object):
             loc.remove()
         self.loc = loc
         self.d = shelve.open(loc)
-        self.fill()
 
     def clear(self):
         cleared = len(self.d)
         self.d.clear()
-        self.fill()
         self.d.sync()
+        return cleared
 
-    def __getattr__(self, name):
-        return getattr(self.d, name)
+    def originals(self):
+        return ((k,v) for k,v in self.iteritems() if v != '!')
+
+    def __iter__(self):
+        return iter(self.d)
+
+    def __setitem__(self, key, item):
+        self.d[key] = item
+
+    def __delitem__(self, key):
+        del self.d[key]
         
+    def __len__(self):
+        return len(self.d)
+
 # provide a persistent dict in a /tmp location by default
 # load allows to specify one in a less ephemeral location
-store = DEFAULTSTORE = Store()
+store = Store()
--- imagy/smushing.py
+++ imagy/smushing.py
@@ -1,4 +1,4 @@
 from smush import Smush
 from config import STRIP_JPG_META
 
-smusher = Smush(strip_jpg_meta=STRIP_JPG_META, exclude=['.bzr', '.git', '.hg', '.svn'], list_only=False, quiet=False, identify_mime=True)
+smusher = Smush(strip_jpg_meta=STRIP_JPG_META, exclude=['.bzr', '.git', '.hg', '.svn'], list_only=False, quiet=True, identify_mime=True)
