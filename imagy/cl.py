d771dfce95bdfb7160594be9793cc9cda55ab396
Dominik Dabrowski <dominik@silberrock.com>
2012-06-18 16:38:29 -0400

    move cli stuff to cl.py

---
+++ imagy/cl.py
@@ -0,0 +1,45 @@
+from core import *
+
+import sys
+import optparse
+
+import logging
+FORMAT = '%(asctime)-15s %(levelname)-12s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
+def main(opts, args):
+    logging.info('Imagy started')
+    logging.info('Ctrl-C to quit')
+    store.load(opts.store_loc)
+    dirs = map(path, args or FILE_PATTERNS)
+    nothing_has_run = None    # False is technincally false here~
+
+    if opts.clear: clear()
+    elif opts.u: dump(store)
+    elif opts.revert: revert()
+    elif opts.init: initialize(*dirs)
+    elif opts.list: list_files()
+    else: nothing_has_run = True
+
+    if opts.run or nothing_has_run:
+        watch.start(dirs)
+    
+if __name__ == "__main__":
+    parser = optparse.OptionParser('Optimize images')
+    parser.add_option('-i', '--init', action="store_true", default=False, help=initialize.__doc__)
+    parser.add_option('-c', '--clear', action="store_true", default=False, help=clear.__doc__)
+    parser.add_option('-l', '--list', action="store_true", default=False, help=list_files.__doc__)
+    parser.add_option('-r', '--revert', action="store_true", default=False, help=revert.__doc__)
+    parser.add_option('-n', '--run', action="store_true", default=False, help='Run the daemon'
+                      'even though another option has been specified')
+    parser.add_option('-u', action="store_true", default=False, help=dump.__doc__)
+    parser.add_option('-d', action="store", default=STORE_LOC, dest="store_loc", help='the folder'
+                      'within which internal storage resides')
+    opts, args = parser.parse_args(sys.argv[1:])
+    try:
+        main(opts, args)
+    finally:
+        try:
+            store.save()
+        except Exception,e:
+            logging.error('unable to save %s', str(e))
