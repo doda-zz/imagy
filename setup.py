68b4f0d537a21bc5c9b20789ef5b3082dc4c521c
Dominik Dario <dominik@doda.co>
2012-06-19 16:54:07 -0400

    pypi please work?

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='imagy',
-    version='0.0.2',
+    version='0.0.4',
     description='A command line tool to automatically handle image optimization',
     author='Dominik Dabrowski',
     author_email='dominik@silberrock.com',
     url='https://github.com/doda/imagy',
     license=open('LICENSE').read(),
-    long_description=open('README').read(),
+    long_description=open('README.rst').read(),
     packages=find_packages(),
     package_data={'': ['README.rst','LICENSE']},
     install_requires=['watchdog','path.py'],
