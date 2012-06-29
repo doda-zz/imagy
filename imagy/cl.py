4e47f11bc3202e0738d05e10a12ba590514ee192
Dominik Dabrowski <dominik@silberrock.com>
2012-06-29 15:52:24 -0400

    Make imagy behave `smartly` by default

--- imagy/cl.py
+++ imagy/cl.py
@@ -16,7 +16,6 @@ FORMAT = '%(asctime)-15s %(levelname)-12s %(message)s'
 parser = optparse.OptionParser('Optimize images')
 
 true_flag = partial(parser.add_option, action="store_true", default=False)
-true_flag('-i', '--init', help=initialize.__doc__)
 true_flag('-c', '--clear', help=clear.__doc__)
 true_flag('-l', '--list', help=list_files.__doc__)
 true_flag('-r', '--revert', help=revert.__doc__)
@@ -25,6 +24,7 @@ true_flag('-q', '--quiet', help='no output')
 true_flag('-m', '--memorystore', help='store internals in memory')
 true_flag('--deloriginals', help=delete_originals.__doc__)
 true_flag('--debug', help='set logging to DEBUG')
+true_flag('--no-init', dest='no_init', help='do not check directories for not yet optimized files')
 
 parser.add_option('-n', '--run', help='Run the daemon'
                   'even though another option has been specified')
@@ -59,13 +59,16 @@ def _main(opts, args):
     if opts.clear: clear()
     elif opts.dump: dump(store)
     elif opts.revert: revert()
-    elif opts.init: initialize(*args)
     elif opts.list: list_files()
     elif opts.files: do_files(*args)
     elif opts.deloriginals: delete_originals()
     else: run_daemon = True
 
     if run_daemon:
+        # if nothing specified so far, just run `smart mode` i.e. initialize the directories
+        # and then run the daemon afterwards
+        if not opts.no_init:
+            initialize(*args)
         watch.start(args)
     
 def main():
