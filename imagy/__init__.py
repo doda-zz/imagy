bb8f92bd8cc656c0cee33a65bce9614d832ad005
Dominik Dabrowski <dominik@silberrock.com>
2012-06-30 11:19:59 -0400

    add basic api, make smush.py wrapper's name more descriptive

---
+++ imagy/__init__.py
@@ -0,0 +1,3 @@
+from core import *
+from store import store, Store
+
---
+++ imagy/libsmush.py
@@ -0,0 +1,24 @@
+'''
+A wrapper around the smush.py library
+
+the library itself has been modifed to make it work with the binaries included in most package managers (to custom patches + recompiling
+'''
+from config import STRIP_JPG_META
+from utils import filesig
+
+from smush import Smush
+from path import path
+
+smusher = Smush(strip_jpg_meta=STRIP_JPG_META, list_only=False, quiet=True, identify_mime=True)
+
+def compress_image(pth, smusher=smusher):
+    '''
+    dirty, but we need to guarantee that the file gets touched to make sure watchdog fires an event
+    when the file gets optimized
+    '''
+    pth = path(pth).abspath()
+    sig = filesig(pth)
+    smusher.smush(pth)
+    if filesig(pth) == sig:
+        # not modified, force it ourselves
+        pth.touch()
