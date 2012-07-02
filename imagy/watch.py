52f4004f2366ae97b227aa4cebab96a7f17409d4
Dominik Dario <dominik@doda.co>
2012-07-01 21:22:19 -0400

    refactor watch into object

--- imagy/watch.py
+++ imagy/watch.py
@@ -31,29 +31,39 @@ class CompressionHandler(FileSystemEventHandler):
         time.sleep(SECONDS_AFTER_CHANGE)
         self.handle_event(event)
 
-def start(dirs):
-    global running
-    running = True
-    event_handler, observer = CompressionHandler(), Observer()
-    for dir in dirs:
-        dir = path(dir).abspath()
-        if dir.isdir():
-            observer.schedule(event_handler, path=dir, recursive=True)
-            logging.warning('watching %s', dir)
-        else:
-            logging.warning('%s is not a directory', dir)
-    if not observer._watches:
-        logging.error('No valid directories specified. Exiting')
-        return
-    
-    observer.start()
-    logging.info('waiting for files')
-    logging.info('Ctrl-C to quit')
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+class Watcher(object):
+    def __init__(self, event_handler_cls, observer_cls=None):
+        if observer_cls is None:
+            self.observer_cls = Observer
+        self.event_handler_cls = event_handler_cls
+        self.running = False
 
+    def add(self, *dirs):
+        dirs = [path(dir).abspath() for dir in dirs]
+        for dir in dirs:
+            if dir.isdir():
+                self.observer.schedule(self.event_handler, path=dir, recursive=True)
+                logging.warning('watching %s', dir)
+            else:
+                logging.warning('%s is not a directory', dir)
 
+    def run(self, *dirs):
+        self.event_handler = self.event_handler_cls()
+        self.observer = self.observer_cls()
+        self.add(*dirs)
+        if not self.observer._watches:
+            logging.error('No valid directories specified.')
+            return
+
+        self.running = True
+        self.observer.start()
+        logging.info('waiting for files')
+        logging.info('Ctrl-C to quit')
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.observer.stop()
+        self.observer.join()
+
+watcher = Watcher(CompressionHandler)
