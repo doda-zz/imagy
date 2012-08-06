8f7f9427aca413900173f1c90e36295bfe9ba6fc
Dominik Dabrowski <dominik@silberrock.com>
2012-08-06 11:32:34 -0400

    remove postprocessing from extensions

--- imagy/config.py
+++ imagy/config.py
@@ -19,16 +19,9 @@ KEEP_ORIGINALS = True
 #this is inserted before the file extension, if the path already exists, append a 0 to the identifier and keep iterating until a free path is found
 ORIGINAL_IDENTIFIER = '-original'
 
-IMAGE_EXTENSIONS = (
-    'jpg',
-    'jpeg',
-    'png',
-    'gif',
-    )
+IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.gif',)
    
 STRIP_JPG_META = True
-
-IMAGE_EXTENSIONS = ['.'+ext for ext in IMAGE_EXTENSIONS]
 
 # the location where imagy stores its internals, if this is `None` at startup, imagy will ask where it should store
 
